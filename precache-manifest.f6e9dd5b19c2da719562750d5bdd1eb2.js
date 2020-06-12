bbda9b0de775f82721a2cfde3fefcc06b4a64d27
yarwinliu <yarwinliu@gmail.com>
2020-06-12 15:27:02 -0700

    first yarwin website build

---
+++ precache-manifest.f6e9dd5b19c2da719562750d5bdd1eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83bab838554cffef81ac537cd75d6d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df068c143eab40a2f61a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25f815dc9e75eb974d98",
+    "url": "/static/js/2.2627c374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2627c374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df068c143eab40a2f61a",
+    "url": "/static/js/main.1458937b.chunk.js"
+  },
+  {
+    "revision": "8f446f4bfdb16c2a7b41",
+    "url": "/static/js/runtime-main.0211362b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
